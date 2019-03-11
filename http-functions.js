4f5604711e5b8a53273a052b35a7fae46baf4976
David <david.m.neuman@gmail.com>
2019-03-11 16:36:21 +0000

    Created final version of 'getHTML' code and saved it as a new module (http-functions.js). 'requireHTTPFunctions.js' uses this module to exemplify that is works properly.

---
+++ http-functions.js
@@ -0,0 +1,36 @@
+var https = require('https');
+var data = '';
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step1.html'
+};
+
+
+
+function getHTML (options, callback) {
+  https.get(options, function(response) {
+    response.setEncoding('UTF-8');
+    response.on('data', function (chunk) {
+      data += chunk;
+    });
+    response.on('error', function (error) {
+      console.log("Error: " + error);
+    });
+    response.on('end', function () {
+      callback(data);
+      console.log("Data read complete.");
+    });
+  });
+};
+
+function printHTML(html) {
+  console.log(html);
+}
+
+module.exports = {
+  getHTML
+}
+
+
+// getHTML(requestOptions, printHTML);
